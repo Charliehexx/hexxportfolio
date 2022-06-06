f89af9d403b9a061efc7dc06c59b4c4707fb0add
Aditya Patel <86345323+Charliehexx@users.noreply.github.com>
2022-06-06 13:22:44 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -18,47 +18,47 @@ export const Experience = [
     id: 1,
     date: "2016 - present",
     iconsSrc: <IoCodeWorking />,
-    title: "Creative Director",
-    location: "Doha, Qatar",
+    title: "Working on Upgrading myself",
+    location: "India",
     description:
-      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+      "Web Developer",
   },
-  {
-    id: 2,
-    date: "2014 - 2016",
-    iconsSrc: <IoCodeWorking />,
-    title: "Creative Director",
-    location: "Doha, Qatar",
-    description:
-      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
-  },
-  {
-    id: 3,
-    date: "2012 - 2014",
-    iconsSrc: <IoCodeWorking />,
-    title: "Creative Director",
-    location: "Doha, Qatar",
-    description:
-      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
-  },
-  {
-    id: 4,
-    date: "2011 - 2012",
-    iconsSrc: <IoCodeWorking />,
-    title: "Creative Director",
-    location: "Doha, Qatar",
-    description:
-      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
-  },
-  {
-    id: 5,
-    date: "2010 - 2011",
-    iconsSrc: <IoCodeWorking />,
-    title: "Creative Director",
-    location: "Doha, Qatar",
-    description:
-      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
-  },
+//   {
+//     id: 2,
+//     date: "2014 - 2016",
+//     iconsSrc: <IoCodeWorking />,
+//     title: "Creative Director",
+//     location: "Doha, Qatar",
+//     description:
+//       "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+//   },
+//   {
+//     id: 3,
+//     date: "2012 - 2014",
+//     iconsSrc: <IoCodeWorking />,
+//     title: "Creative Director",
+//     location: "Doha, Qatar",
+//     description:
+//       "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+//   },
+//   {
+//     id: 4,
+//     date: "2011 - 2012",
+//     iconsSrc: <IoCodeWorking />,
+//     title: "Creative Director",
+//     location: "Doha, Qatar",
+//     description:
+//       "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+//   },
+//   {
+//     id: 5,
+//     date: "2010 - 2011",
+//     iconsSrc: <IoCodeWorking />,
+//     title: "Creative Director",
+//     location: "Doha, Qatar",
+//     description:
+//       "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+//   },
 ];
 
 export const Projects = [
