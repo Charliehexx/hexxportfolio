5f0568095a9581bf8062fc6a91fbaa3293e9d907
Aditya Patel <86345323+Charliehexx@users.noreply.github.com>
2022-06-25 20:53:55 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -73,7 +73,7 @@ export const Projects = [
   },
   {
     id: 2,
-    name: "React-Firebase Messenger Chat App,
+    name: "React-Firebase Messenger Chat App",
     imageSrc: img2,
     techs: "React Js,Firebase",
     github: "https://github.com/Charliehexx/react-messenger",
